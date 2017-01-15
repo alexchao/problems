52b01316e2007eff54077d2de063eaca3a0be461
Alex Chao <alex.h.chao@gmail.com>
2017-01-15 15:23:14 +0800

    group anagrams

--- string_problems.py
+++ string_problems.py
@@ -8,3 +8,21 @@ def reverse_words(s):
         return sentence
     return reverse_words(sentence[space_index + 1:]) + ' ' \
             + sentence[:space_index]
+
+
+def group_anagrams(list_of_words):
+
+    def _word_to_id(w):
+        word_lower = w.lower()
+        sorted_chars = sorted(word_lower, key=ord)
+        return ''.join(sorted_chars)
+
+    id_to_words = {}
+    for word in list_of_words:
+        word_id = _word_to_id(word)
+        if word_id not in id_to_words:
+            id_to_words[word_id] = [word]
+        else:
+            id_to_words[word_id].append(word)
+
+    return [set(v) for k, v in id_to_words.items()]
--- string_problems_test.py
+++ string_problems_test.py
@@ -3,6 +3,7 @@ import unittest
 
 
 from string_problems import reverse_words
+from string_problems import group_anagrams
 
 
 class ReverseWordsTest(unittest.TestCase):
@@ -18,6 +19,17 @@ class ReverseWordsTest(unittest.TestCase):
 
     def test_reverse_whitespace(self):
         assert reverse_words(' alex chao ') == 'chao alex'
+
+
+class GroupAnagramsTest(unittest.TestCase):
+
+    def test_group_anagrams(self):
+        words = ['het', 'costar', 'the', 'bottle', 'eht', 'actors']
+        groups = group_anagrams(words)
+        assert len(groups) == 3
+        assert set(['the', 'het', 'eht']) in groups
+        assert set(['actors', 'costar']) in groups
+        assert set(['bottle']) in groups
 
 
 if __name__ == '__main__':
