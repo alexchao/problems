f82b2e92eefd3b57af7dad6b27efeee289096de5
Alex Chao <alex.h.chao@gmail.com>
2017-01-23 10:12:07 -0800

    add_two_numbers refactor: remove _copy

--- python/linked_list_problems.py
+++ python/linked_list_problems.py
@@ -10,30 +10,26 @@ class ListNode(object):
 
 def add_two_numbers(l1, l2):
 
-    def _copy(list_node, is_carry_over):
-        if not list_node:
-            if is_carry_over:
-                return ListNode(1)
-            return None
-        digit_sum = list_node.val + (1 if is_carry_over else 0)
-        next_has_carry_over = digit_sum >= 10
-        l = ListNode(digit_sum - 10 if next_has_carry_over else digit_sum)
-        l.next = _copy(list_node.next, next_has_carry_over)
-        return l
+    def _get_val_from_digit(list_node):
+        if list_node:
+            return list_node.val
+        return 0
 
     def _add(l1, l2, is_carry_over):
         if l1 is None and l2 is None:
             if is_carry_over:
                 return ListNode(1)
             return None
-        if l1 is None:
-            return _copy(l2, is_carry_over)
-        if l2 is None:
-            return _copy(l1, is_carry_over)
-        digit_sum = l1.val + l2.val + (1 if is_carry_over else 0)
+
+        digit_sum = _get_val_from_digit(l1) + \
+                _get_val_from_digit(l2) + \
+                (1 if is_carry_over else 0)
         next_has_carry_over = digit_sum >= 10
         l = ListNode(digit_sum - 10 if next_has_carry_over else digit_sum)
-        l.next = _add(l1.next, l2.next, next_has_carry_over)
+        l.next = _add(
+            l1.next if l1 else None,
+            l2.next if l2 else None,
+            next_has_carry_over)
         return l
 
     return _add(l1, l2, False)
