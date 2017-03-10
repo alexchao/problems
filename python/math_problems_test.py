439bfebf3b2a36576cace80b862bd1f86211b3f5
Alex Chao <alex.h.chao@gmail.com>
2017-03-09 17:32:53 -0800

    math problems tests: use unittest assertions

--- python/math_problems_test.py
+++ python/math_problems_test.py
@@ -11,25 +11,25 @@ from math_problems import is_power_of_three
 class IsPowerOfThreeTest(unittest.TestCase):
 
     def test_zero(self):
-        assert is_power_of_three(0) == False
+        self.assertFalse(is_power_of_three(0))
 
     def test_one(self):
-        assert is_power_of_three(1) == True
+        self.assertTrue(is_power_of_three(1))
 
     def test_three(self):
-        assert is_power_of_three(3) == True
+        self.assertTrue(is_power_of_three(3))
 
     def test_basic(self):
-        assert is_power_of_three(9) == True
-        assert is_power_of_three(27) == True
-        assert is_power_of_three(81) == True
-        assert is_power_of_three(243) == True
+        self.assertTrue(is_power_of_three(9))
+        self.assertTrue(is_power_of_three(27))
+        self.assertTrue(is_power_of_three(81))
+        self.assertTrue(is_power_of_three(243))
 
     def test_multiples_but_not_powers(self):
-        assert is_power_of_three(12) == False
-        assert is_power_of_three(18) == False
-        assert is_power_of_three(99) == False
-        assert is_power_of_three(120) == False
+        self.assertFalse(is_power_of_three(12))
+        self.assertFalse(is_power_of_three(18))
+        self.assertFalse(is_power_of_three(99))
+        self.assertFalse(is_power_of_three(120))
 
 
 class CountStaircaseClimbTest(unittest.TestCase):
